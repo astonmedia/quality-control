5e6d82d6a6d9ac91598e5c1bdc15062a45f826b6
astonmedia <astonmedia@hotmail.com>
2023-11-16 05:24:58 +1000

    set up api end points

---
+++ src/app/api/route.ts
@@ -0,0 +1,6 @@
+export async function GET(request: Request) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return Response.json({ data });
+}
