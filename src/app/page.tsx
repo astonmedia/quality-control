50f4bc158ec40fec7cb6b350a1f2fdd8e99e4326
astonmedia <astonmedia@hotmail.com>
2023-11-28 05:57:13 +1000

    design for front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
         <div>
           <Sidenav />
         </div>
-        <div className="col-span-9"></div>
+        <div className="col-span-9">
+          <div className="flex flex-col max-h-screen">
+            <div className="bg-slate-300 w-full h-full"></div>
+            <div className="bg-slate-600 w-full h-full"></div>
+          </div>
+        </div>
       </div>
     </main>
   );
