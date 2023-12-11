445ffbccb71698f1ac19c373e2c0cc3a928ec5a2
astonmedia <astonmedia@hotmail.com>
2023-12-12 05:13:17 +1000

    added signet yellow to tailwind config

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,25 @@
 "use client";
+import React from "react";
 import { useRouter } from "next/navigation";
 const Login = () => {
   const router = useRouter();
-  const onSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     router.push("/");
   };
 
   return (
     <div className="flex flex-row min-h-screen justify-center items-center">
-      <div className="bg-white lg:w-6/12 md:w-7/12 w-8/12 shadow-3xl rounded-xl">
+      <div className="bg-signet_yellow lg:w-6/12 md:w-7/12 w-8/12 shadow-3xl rounded-xl">
         <div className="bg-gray-800 shadow shadow-gray-200 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
           <svg width="32" height="32" viewBox="0 0 24 24" fill="#FFF">
             <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
           </svg>
         </div>
         <form onSubmit={(e) => onSubmit(e)} className="p-12 md:p-24">
+          <div className="text-black flex justify-center mx-auto mb-6">
+            <h1 className="text-2xl font-bold">Signet Quality System</h1>
+          </div>
           <div className="flex items-center text-lg mb-6 md:mb-8 text-black">
             <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
               <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
